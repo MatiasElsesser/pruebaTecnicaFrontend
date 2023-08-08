16987f8d3f2ece92edec93384a66e84d7763ecb2
ElsesserMatias <elsesser3@gmail.com>
2023-08-07 21:15:21 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain='dev-lr81zxxjkjt6hs83.us.auth0.com'
+      clientId='n6ajTanmnBOEhhIuZLeq7T0tn1DDlBip'
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+)
